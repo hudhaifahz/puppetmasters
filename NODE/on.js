d302fe83c395465c6df68a56d9000062cdc56cf4
Hudhaifah Zahid <hudhaifahz@gmail.com>
2017-02-04 13:06:39 -0800

    create on.js

---
+++ NODE/on.js
@@ -0,0 +1,8 @@
+module.exports = function () {
+  var exec = require('child_process').exec;
+  exec('gpio write 9 1', function(err, stdout, stderr) {
+    if (err) throw err
+    process.stdout.write(stdout)
+    process.stderr.write(stderr)
+    })
+}
