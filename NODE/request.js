55224408bb8766e8260280a0151549c4677a3ae7
Hudhaifah Zahid <hudhaifahz@gmail.com>
2017-02-04 09:49:56 -0800

    upload NODE content

---
+++ NODE/request.js
@@ -0,0 +1,55 @@
+var http = require('http');						//allows server
+var exec = require('child_process').exec;				//allows terminal commands
+//for html test
+var fs = require('fs');							//allows reading of file, only for test
+fs.readFile('./index.html', function (err, html) {
+  if (err) {
+      throw err; 
+  }
+
+http.createServer(function(request, response) {				//starts server
+//for html test
+  response.writeHeader(200, {"Content-Type": "text/html"});		
+  response.write(html);
+  response.end();
+
+  request.on('error', function(err) {
+    console.error(err);
+    response.statusCode = 400;
+    response.end();
+  });
+  response.on('error', function(err) {
+    console.error(err);
+  });
+  if (request.method === 'GET' && request.url === '/') {
+//    response.writeHeader(200, {"Content-Type": "text/html"});
+//    response.write(html);
+//    response.end();
+//    request.pipe(response);
+//    response.writeHead(200, {"Content-Type": "text/plain"});
+//    response.end("Hello World\n");
+  }
+  else if (request.method === 'GET' && request.url === '/?echo%2Fon='){
+    exec('gpio write 9 1', function(err, stdout, stderr) {
+    if (err) throw err
+    process.stdout.write(stdout)
+    process.stderr.write(stderr)
+    })
+    response.end("ON\n");
+  }
+  else if (request.method === 'GET' && request.url === '/?echo%2Foff='){
+    exec('gpio write 9 0', function(err, stdout, stderr) {
+    if (err) throw err
+    process.stdout.write(stdout)
+    process.stderr.write(stderr)
+    })
+    response.end("OFF\n");
+  } 
+  else {
+    response.statusCode = 404;
+    response.end();
+  }
+}).listen(8080);
+
+});
+
