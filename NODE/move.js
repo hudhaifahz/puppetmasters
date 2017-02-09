dfe4d73c982e3670f0877fed67e0850088783c94
David Liu <davidliu@live.com>
2017-02-09 03:00:49 -0800

    altered code structure to fit the new requirement; implemented part of motor control for testing

--- NODE/move.js
+++ NODE/move.js
@@ -1,50 +1,29 @@
-module.exports = function (limb, speed, direction) {
+module.exports = function (outPin, speed) {
   var pwmValue = 0;
-  var pin;
-  //input security check
-  if(0<=limb<=17 && Number.isInteger(limb)){
-  	if(speed === "fast" || speed === "mid" || speed === "slow"){
-  	  if(direction === "up" || direction === "down"){
+  var pin = outPin;
 
-		  if(speed === 'fast'){
-		  	pwmValue = 1023; 	//full speed
-		  }
-		  else if(speed === 'mid'){
-		  	pwmValue = 600;
-		  }
-		  else{
-		  	pwmValue = 200;
-		  }
-
-		  //TODO: Convert limb# into output pin, depends on limb# and direction and the wiring
-
-		  //		Suppose for each motor we need 2 pins to control it: for example, for limb
-		  //		#02 (right upper arm), we are using pin 2 and pin 3. In this case, to move
-		  //		limb up we give pin 2 positive output and pin 3 0 (GND), and to move limb down
-		  //		we give pin 2 0 (GND) and pin 3 positive output
-		  
-		  //TODO: Add timeout for moves, restrict each move to a certain amount of time (maybe
-		  //		1 second). This is a safty feature.
-
-
-		  if(direction === 'up'){
-			  var exec = require('child_process').exec; 
-			  exec('gpio pwm ' + pin + ' ' + pwmValue, function(err, stdout, stderr) {
-			    if (err) throw err
-			    process.stdout.write(stdout)
-			    process.stderr.write(stderr)
-			    })
-		  }
-		  else{
-		  	  pin = pin + 1;
-		  	  var exec = require('child_process').exec; 
-			  exec('gpio pwm ' + pin + ' ' + pwmValue, function(err, stdout, stderr) {
-			    if (err) throw err
-			    process.stdout.write(stdout)
-			    process.stderr.write(stderr)
-			    })
-		  }	    	
-  	  }
-  	}
+  if(speed>=0){	
+  	  speed = Math.min(speed, 127);
+  	  pwmValue = 1023 * (speed / 127);
+	  var exec = require('child_process').exec; 
+	  exec('gpio pwm ' + pin + ' ' + pwmValue, function(err, stdout, stderr) {
+	    if (err) throw err
+	    process.stdout.write(stdout)
+	    process.stderr.write(stderr)
+	    })
   }
+  else{
+  	//TODO: Decide what to do for negative input (move down) - see detail problem description in request.js
+  	//		For now, temporarily putting a place holder that let it does the same as positive input
+  	  speed = -1 * Math.max(speed, -127);
+  	    	  pwmValue = 1023 * (speed / 127);
+	  var exec = require('child_process').exec; 
+  	  exec('gpio pwm ' + pin + ' ' + pwmValue, function(err, stdout, stderr) {
+	    if (err) throw err
+	    process.stdout.write(stdout)
+	    process.stderr.write(stderr)
+	    })
+  }	
+
+
 }
--- NODE/request.js
+++ NODE/request.js
@@ -7,6 +7,12 @@ var pinmap = {  '/?echo%2Fon=': 9,
                 '/?echo%2Foff=': 9, 
                 '/': [9,'gpio write 9 1'] };
 
+// Admin code
+// For easy remembering: "ALICE" with each letter changed to the key above(left) it on standard keyboard
+var adminCode = "QO8D3" 
+// Default guest code, can be changed by admin
+var guestCode = "00000"
+
 // for html test
 var fs = require('fs');							//allows reading of file, only for test
 fs.readFile('./index.html', function (err, html) {
@@ -28,84 +34,86 @@ http.createServer(function(request, response) {				//starts server
   response.on('error', function(err) {
     console.error(err);
   });
-<<<<<<< HEAD
-  if (request.method === 'GET' && request.url === '/') {    
-    var a = request.url;
-    var i = pinmap[a];
-    console.log(a);
-    console.log(i);
-	console.log(i[0]);
-	console.log(i[1]);
-    var ontest = require('./ontest.js');
-    ontest(i[1]);
-//  exec( './blink' , function(err, stdout, stderr) {
-//  if (err) throw err
-//  process.stdout.write(stdout)
-//  process.stderr.write(stderr)
-//  });
-    response.end();
-    
 
-//    response.writeHeader(200, {"Content-Type": "text/html"});
-//    response.write(html);
-//    response.end();
-//    request.pipe(response);
-//    response.writeHead(200, {"Content-Type": "text/plain"});
-//    response.end("Hello World\n");
-  }
-  else if (request.method === 'GET' && request.url === '/?echo%2Fon='){
-=======
+  // URL format: http://puppet/XYE58/arm/right/127    127 is the speed control input value
+  // reqUrl format: ",XYE58,arm,right,127" 
+  // Note: reqUrl[0]="" reqUrl[1]="XYE58" reqUrl[2]="arm" reqUrl[3]="right" reqUrl[4]=127
+
+  var reqUrl = request.url.replace('%2F','/').split('/')
+
   if (request.method === 'GET') { 
-    if (request.url === '/'){   
-      var a = request.url;
-      var i = pinmap[a];
-      console.log(a);
-      console.log(i);
-  	console.log(i[0]);
-  	console.log(i[1]);
-      var ontest = require('./ontest.js');
-      ontest(i[0]);
-  //  exec( './blink' , function(err, stdout, stderr) {
-  //  if (err) throw err
-  //  process.stdout.write(stdout)
-  //  process.stderr.write(stderr)
-  //  });
-      response.end();
+    if (reqUrl.length == 1){   
+      //TODO: Default page, a form asking for the user's auth code
     }  
-    else if (request.url === '/?echo%2Fon='){
->>>>>>> origin/master
-      var on = require('./on.js');
-      on();
-//    exec('gpio write 9 1', function(err, stdout, stderr) {
-//    if (err) throw err
-//    process.stdout.write(stdout)
-//    process.stderr.write(stderr)
-//    })
-      response.end("ON\n");
+    else if (reqUrl.length == 2 && reqUrl[1] === adminCode){
+      //TODO: Admin page, user gets another auth code that can be handed out to other 
+      //      users, plus a button to generate another code.  This non-hardcoded auth 
+      //      code is good for X minutes (needs to be configurable). Guest code should 
+      //      go in a file that can be checked to see if it matches and if it is still valid.
     }
-    else if (request.url === '/?echo%2Foff='){
-      var off = require('./off.js');
-      off();
-//    exec('gpio write 9 0', function(err, stdout, stderr) {
-//    if (err) throw err
-//   process.stdout.write(stdout)
-//   process.stderr.write(stderr)
-//    })
-      response.end("OFF\n");
+    else if (reqUrl.length == 2 && reqUrl[1] === guestCode){
+      //TODO: Guest logged in page (is this needed???)
     }
-    else if (request.url.startsWith("/?echo%2Fmove")){
-      // sample: '/?echo%2Fmove%2F02fastup=' <-- limb #02 move up, fast speed
+    else if (reqUrl.length == 5){
+      // check if the guest authentication code is correct
+      if(reqUrl[1] != guestCode){
+        response.statusCode = 401;
+        response.end();
+        break;
+      }
+      //TODO: Movement control, map body parts to pin. Add timeout.
+      //      Problem: Only 17 GPIO PWM pins and output is 0-1023. How to do move up/down?
+      //               Use something else instead of PWM? Do motor relays support negative input?
+      //               2 sets of Pi, switch 2 input/GND pins to make motor turn reverse?
+      //A temporary design (until problem mentioned above is resolved):
+      //    Head Right = pin 11     <---- why do we have head left and head right? what do they do?
+      //    Head Left = 12
+      //    ShoulderFront Right = 13
+      //    ShoulderFront Left = 15
+      //    ShoulderBack  Right = 16
+      //    ShoulderBack  Left = 18
+      //    Arm Right = 22 
+      //    Arm Left = 29
+      //    Hand Right = 31
+      //    Hand Left = 32
+      //    Fingers Right = 33
+      //    Fingers Left = 35
+      //    Hips = 7     <--- this will need a seperate else if condition
+      //    Leg Right = 36
+      //    Leg Left = 37
+      //    Foot Right = 38
+      //    Foot Left = 40
+      var outPin = 29;  // default, least likely to cause safty problem in case of bug
+      var speed = parseInt(reqUrl[4]);
+      breakSwitch: switch(reqUrl[2]){
+        //TODO: finish inplementation for the rest of the cases
+        case "arm":
+            switch(reqUrl[3]){
+                case "right":
+                    outPin = 22;
+                    break breakSwitch;
+                case "left":
+                    outPin = 29;
+                    break breakSwitch;
+                default:  //invalid command  
+                    response.statusCode = 404;
+                    response.end();
+                    break breakSwitch;
+            }
 
-      // TODO: verify the url styling about limb, speed and direction information
+        default:  //invalid command
+                   response.statusCode = 404;
+                   response.end();
 
-      var req = request.url;
-      var limb = parseInt(req.substring(16, 18));
-      var speed = req.substring(18, 22);
-      var direction = req.substring(22, 24);
-      var move = require('./move.js');       //TODO: finish implement of move.js
-      move(limb, speed, direction);
+      }
+      var move = require('./move.js'); 
+      //TODO: change implemenattion of move.js      
+      move(outPin, speed);
       response.end("MOVED\n");
+
+
     }
+
     else {
       response.statusCode = 404;
       response.end();
