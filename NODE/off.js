e9eed0f8223810038180df0e228e488907dea06c
hudhaifahz <hudhaifahz@gmail.com>
2017-02-26 20:13:41 -0800

    cleaned up Code

--- NODE/off.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-  var exec = require('child_process').exec;
-  exec('gpio write 1 0', function(err, stdout, stderr) {
-    if (err) throw err
-    process.stdout.write(stdout)
-    process.stderr.write(stderr)
-    })
-}
-
